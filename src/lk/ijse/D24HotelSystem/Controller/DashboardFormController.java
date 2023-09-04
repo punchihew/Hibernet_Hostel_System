193e1ba58284d05b2d8e20b2eb0dc52dd13a9d8f
punchihewa <navishkapunchihewa66@gmail.com>
2023-09-04 22:23:27 +0530

    1.3

--- src/lk/ijse/D24HotelSystem/Controller/DashboardFormController.java
+++ src/lk/ijse/D24HotelSystem/Controller/DashboardFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.D24HotelSystem.Controller;
 
+import javafx.event.ActionEvent;
+
 public class DashboardFormController {
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signOutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void setttingsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void reservationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void studentsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void roomsOnAction(ActionEvent actionEvent) {
+    }
 }
