a019f7d5fe5130ab1ce138cd3abc365938c0e265
punchihewa <navishkapunchihewa66@gmail.com>
2023-09-05 14:44:33 +0530

    1.37

--- src/lk/ijse/D24HotelSystem/Dao/custom/QueryDAO.java
+++ src/lk/ijse/D24HotelSystem/Dao/custom/QueryDAO.java
@@ -1,7 +1,7 @@
 package lk.ijse.D24HotelSystem.Dao.custom;
 
-import lk.ijse.d24hostelsystem.dao.SuperDAO;
-import lk.ijse.d24hostelsystem.projection.StudentDetailsDTO;
+import lk.ijse.D24HotelSystem.Dao.SuperDAO;
+import lk.ijse.D24HotelSystem.Projection.StudentDetailsDTO;
 
 import java.util.List;
 
