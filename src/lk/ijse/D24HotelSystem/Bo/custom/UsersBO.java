97d4c4f5ac9a124f1ee13a6cb3af530a39b33356
punchihewa <navishkapunchihewa66@gmail.com>
2023-09-05 14:59:19 +0530

    1.36

---
+++ src/lk/ijse/D24HotelSystem/Bo/custom/UsersBO.java
@@ -0,0 +1,17 @@
+package lk.ijse.D24HotelSystem.Bo.custom;
+
+
+
+import lk.ijse.d24hostelsystem.bo.SuperBO;
+import lk.ijse.d24hostelsystem.dto.UsersDTO;
+
+import java.util.List;
+
+public interface UsersBO extends SuperBO {
+    List<UsersDTO> loadAll() throws Exception;
+    boolean saveUsers(UsersDTO usersDTO) throws Exception;
+    boolean updateUsers(UsersDTO usersDTO) throws Exception;
+    boolean deleteUsers(UsersDTO usersDTO) throws Exception;
+
+    String generateNextUserID() throws Exception;
+}
